05eed6c24fd6cdf027921b966008f08b1756b19d
Jacob Bloemker <jbloemker97@gmail.com>
2018-08-19 19:18:36 -0400

    Repeat Same Item Multiple Times

---
+++ repeat_same_item.py
@@ -0,0 +1,7 @@
+'''
+Create a function that takes two arguments (item, times). The first argument (item) is the item that needs repeating while the second argument (times) is the number of times the item is to be repeated. Return the result in a list.
+'''
+
+
+def repeat(item, times):
+	return [item for i in range(times)]
